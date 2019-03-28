37e5b968ffb8ae304142450cddd471b67907c709
BPYap <32807097+BPYap@users.noreply.github.com>
2019-03-29 00:10:09 +0800

    Integrate tweets query to web interface

--- web_interface/web_interface/views.py
+++ web_interface/web_interface/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 
+from indexer.indexing import query as elastic_query
+
 
 def home(request):
-    return render(request, "index.html", {'name': "Home"})
+    outputs = []
+    if "text_input" in request.POST and request.POST["text_input"] != "":
+        # Get inputs from front-end UI and pre-process the input if necessary
+        query = request.POST["text_input"]
+        outputs.append({query: [str(elastic_query(match_word=query))]})  # TODO: parse JSON to display in tabular form
+
+    return render(request, "index.html", {'name': "Home", "outputs": outputs})
 
 
 def corpus(request):
