5c44863962ff4ae135ac5b1d58ca68a6246b567a
BPYap <32807097+BPYap@users.noreply.github.com>
2019-04-09 10:42:50 +0800

    Refactor code

--- build/lib/indexer/main.py
+++ build/lib/indexer/main.py
@@ -1,15 +1,31 @@
+import csv
+
 from indexing import create_index, query
 
+QUERY = "trump"
 
 create_index()
-results = []
 search_after_id = None
+file = None
+writer = None
 while True:
-    response = query(match_word="trump", search_after=search_after_id)
-    temp = response['results']
-    if len(temp) == 0:
+    response = query(match_word=QUERY, search_after=search_after_id)
+    if file is None:
+        total_results = response["total_result"]
+        time_taken = response["time_taken"]
+        file = open("query={},total_result={},time_taken={}s.csv".format(QUERY, total_results, time_taken),
+                    'a', encoding='utf-8', newline="")
+        writer = csv.writer(file)
+        writer.writerow(["Fullname", "URL", "Timestamp", "Content", "Replies", "Retweets", "Likes", "Sentiment"])
+
+    tweets = response['results']
+    if len(tweets) == 0:
         break
-    results.extend(temp)
+    else:
+        for tweet in tweets:
+            writer.writerow([tweet.fullname, tweet.url, tweet.timestamp, tweet.content,
+                             tweet.num_reply, tweet.num_retweet, tweet.num_like, tweet.sentiment])
+
     search_after_id = response['search_after']
 
-print(len(results))
+file.close()
--- indexer/main.py
+++ indexer/main.py
@@ -1,15 +1,30 @@
-from indexing import create_index, query
+import csv
 
+from indexing import query
 
-create_index()
-results = []
+QUERY = "trump"
+
 search_after_id = None
+file = None
+writer = None
 while True:
-    response = query(match_word="trump", search_after=search_after_id)
-    temp = response['results']
-    if len(temp) == 0:
+    response = query(match_word=QUERY, search_after=search_after_id)
+    if file is None:
+        total_results = response["total_result"]
+        time_taken = response["time_taken"]
+        file = open("../data/query={},total_result={},time_taken={}s.csv".format(QUERY, total_results, time_taken),
+                    'a', encoding='utf-8', newline="")
+        writer = csv.writer(file)
+        writer.writerow(["Fullname", "URL", "Timestamp", "Content", "Replies", "Retweets", "Likes", "Sentiment"])
+
+    tweets = response['results']
+    if len(tweets) == 0:
         break
-    results.extend(temp)
+    else:
+        for tweet in tweets:
+            writer.writerow([tweet.fullname, tweet.url, tweet.timestamp, tweet.content,
+                             tweet.num_reply, tweet.num_retweet, tweet.num_like, tweet.sentiment])
+
     search_after_id = response['search_after']
 
-print(len(results))
+file.close()
